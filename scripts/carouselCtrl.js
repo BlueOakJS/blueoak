e0d6f78883a37a1e6e767b6dbc1538a758be69bd
Brant Wellons <brant.wellons@pointsource.com>
2017-01-05 11:04:44 -0500

    Remove call to webservice for live data

--- scripts/carouselCtrl.js
+++ scripts/carouselCtrl.js
@@ -13,19 +13,19 @@ app.controller("carouselCtrl", function($http, $scope) {
     }
 
     function getXMLRssFeed(callback) {
-    	$http({
-    		url: 'http://cors-passthrough.herokuapp.com/?url=https://www.pointsource.com/feed/&responseType=XML',
-    		method: 'GET',
-    		transformResponse: function(data) {
-                // string -> XML document object
-                return $.parseXML(data);
-            }
-    	})
-		.then((res) => {
-			callback(null, res.data);
-		}, (err) => {
-			callback(err, null);
-		});
+  //   	$http({
+  //   		url: 'http://cors-passthrough.herokuapp.com/?url=https://www.pointsource.com/feed/&responseType=XML',
+  //   		method: 'GET',
+  //   		transformResponse: function(data) {
+  //               // string -> XML document object
+  //               return $.parseXML(data);
+  //           }
+  //   	})
+		// .then((res) => {
+		// 	callback(null, res.data);
+		// }, (err) => {
+		// 	callback(err, null);
+		// });
     }
 
     function buildArticles(data) {
@@ -33,7 +33,7 @@ app.controller("carouselCtrl", function($http, $scope) {
 
     	let nodeCount = data.documentElement.childNodes[1].childNodes.length;
     	let usableNodes = data.documentElement.childNodes[1].childNodes;
-        console.log('a', nodeCount, usableNodes)
+        
     	for (let i = 0; i < nodeCount; i++) {
     		if (usableNodes[i].nodeName === 'item') { // Item represents article from WP
     			$scope.articles.length < 3 && $scope.articles.push({
